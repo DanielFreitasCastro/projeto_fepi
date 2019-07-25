dfe3d0d91c6b54f6089aabdaaec13986e75ea36a
Castro <dfcastro321@gmail.com>
2019-07-25 19:51:23 -0300

    Ajustes Path Deploy React

--- app_react/src/App.js
+++ app_react/src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './routes';
+import { createBrowserHistory } from 'history';
+
+export const history = createBrowserHistory({
+  basename: process.env.PUBLIC_URL
+});
 
 function App() {
   return (
